afc229ed1f698877e6b31455813c5ca3e50e26cc
jonathan_uber <jonathanuber121207@gmail.com>
2025-08-20 21:15:46 -0300

    #55 fix: printa nome de usuario na tela de login

--- pages/perfil/script.js
+++ pages/perfil/script.js
@@ -1,6 +1,9 @@
 // Mostra o nome do usuário, link para página inicial e botão de sair
 window.addEventListener("DOMContentLoaded", function() {
   const nome = localStorage.getItem("nome_usuario") || "Usuário";
-  const nomeEl = document.getElementById("nomeAluno");
-  if (nomeEl) nomeEl.textContent = nome + "!";
+  const email = localStorage.getItem("email_usuario") || "aluno@gmail.com";
+  const nomeEl = document.querySelector(".nome-email h1");
+  const emailEl = document.querySelector(".nome-email p");
+  if (nomeEl) nomeEl.textContent = nome;
+  if (emailEl) emailEl.textContent = email;
 });
